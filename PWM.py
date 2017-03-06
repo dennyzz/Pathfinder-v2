8f58a2e7d0038ca72562b07004723d0ddaa8b817
Denny Zhang <dennyz@umich.edu>
2017-03-06 17:23:43 -0500

    PWM file for testing on the pi

---
+++ PWM.py
@@ -0,0 +1,27 @@
+from RPIO import PWM
+
+# Setup PWM and DMA channel 0
+PWM.setup()
+PWM.init_channel(0)
+
+# Add some pulses to the subcycle
+PWM.add_channel_pulse(0, 17, 0, 50)
+PWM.add_channel_pulse(0, 17, 100, 50)
+
+# Stop PWM for specific GPIO on channel 0
+PWM.clear_channel_gpio(0, 17)
+
+# Shutdown all PWM and DMA activity
+PWM.cleanup()
+
+
+servo = PWM.Servo()
+
+# Set servo on GPIO17 to 1200µs (1.2ms)
+servo.set_servo(17, 1200)
+
+# Set servo on GPIO17 to 2000µs (2.0ms)
+servo.set_servo(17, 2000)
+
+# Clear servo on GPIO17
+servo.stop_servo(17)
