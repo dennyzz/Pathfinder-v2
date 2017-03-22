ea3f81fd7585844e7383f601c485463ef673aa6a
Pathfinder Project <Pathfinder@umich.edu>
2017-03-22 14:00:13 -0500

    something working for motor control

--- motortest.py
+++ motortest.py
@@ -8,23 +8,26 @@ import smbus
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 DEVICE_ADDRESS = 0x60      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_LED_BASE = 0x06
-DEVICE_REG_MOT1A = 0x06+8
-DEVICE_REG_MOT1B = 0x06+12
+DEVICE_REG_MOTPWM = 0x06+8*4
+DEVICE_REG_MOT1A = 0x06+9*4
+DEVICE_REG_MOT1B = 0x06+10*4
+
 PCA9685_MODE1 = 0x0
 PCA9685_PRESCALE = 0xFE
 PCA_MODE = 0x21
 bus.write_byte_data(DEVICE_ADDRESS, PCA9685_MODE1, PCA_MODE | 0x10)
 bus.write_byte_data(DEVICE_ADDRESS, PCA9685_PRESCALE, 0x06)
 bus.write_byte_data(DEVICE_ADDRESS, PCA9685_MODE1, PCA_MODE)
-pwm_values = [0x00, 0x00, 0xFF, 0xFF]
+pwm_on = [0x00, 0x10, 0xFF, 0x0F]
+pwm_off = [0x00, 0x00, 0xFF, 0x1F]
 while(True):
     #Write a single register
     # bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, 0x80)
 
-    #Write an array of registers
-    pwm_values = [0xAA, 0xAA, 0xFF, 0xFF]
-    for x in range(0, 15):
-        bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_LED_BASE + 4*x, pwm_values)
+    #Write an array of
+    bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_MOT1A, pwm_on)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_MOTPWM, pwm_on)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, DEVICE_REG_MOT1B, pwm_off)
     
 
 # void Adafruit_MS_PWMServoDriver::setPWM(uint8_t num, uint16_t on, uint16_t off) {
