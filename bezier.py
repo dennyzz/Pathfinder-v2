0219589b219a998f85c3cd18529544d7588861fb
Denny <dennyz@umich.edu>
2017-03-23 22:54:43 -0400

    some tweaking of th bezier curve algo

--- bezier.py
+++ bezier.py
@@ -5,20 +5,19 @@ import os
 import scipy.signal
 import sys
 
-# cap = cv2.VideoCapture("footage/radius2angle75.mp4")
+cap = cv2.VideoCapture("footage/radius2angle75.mp4")
 # cap = cv2.VideoCapture("footage/0degree.mp4 ")
-cap = cv2.VideoCapture("footage/rootbeercar.mp4 ")
-fps = cap.get(cv2.CAP_PROP_FPS)
+# cap = cv2.VideoCapture("footage/rootbeercar.mp4 ")
+# fps = cap.get(cv2.CAP_PROP_FPS)
 
 # print(fps)
-w = -1/105
-b = 1/105
-smooth_time = 0;
+w = 1/600
+b = -1/600
+smooth_time = 0
 # block_5_left = np.array([[b,b,b,b,b], [b,b,b,b,w], [b,b,b,w,w], [b,b,w,w,w], [b,w,w,w,w]])
 # block_5_right = np.array([[b,b,b,b,b], [w,b,b,b,b], [w,w,b,b,b], [w,w,w,b,b], [w,w,w,w,b]])
 
 block_15_left = np.array([
-[b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
 [b,b,b,b,b,b,b,b,b,b,b,b,b,b,w],
 [b,b,b,b,b,b,b,b,b,b,b,b,b,w,w],
 [b,b,b,b,b,b,b,b,b,b,b,b,w,w,w],
@@ -32,10 +31,10 @@ block_15_left = np.array([
 [b,b,b,b,w,w,w,w,w,w,w,w,w,w,w],
 [b,b,b,w,w,w,w,w,w,w,w,w,w,w,w],
 [b,b,w,w,w,w,w,w,w,w,w,w,w,w,w],
-[b,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
+[b,w,w,w,w,w,w,w,w,w,w,w,w,w,w],
+[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
 ])
 block_15_right = np.array([
-[b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
 [w,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
 [w,w,b,b,b,b,b,b,b,b,b,b,b,b,b],
 [w,w,w,b,b,b,b,b,b,b,b,b,b,b,b],
@@ -49,7 +48,8 @@ block_15_right = np.array([
 [w,w,w,w,w,w,w,w,w,w,w,b,b,b,b],
 [w,w,w,w,w,w,w,w,w,w,w,w,b,b,b],
 [w,w,w,w,w,w,w,w,w,w,w,w,w,b,b],
-[w,w,w,w,w,w,w,w,w,w,w,w,w,w,b]
+[w,w,w,w,w,w,w,w,w,w,w,w,w,w,b],
+[w,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
 ])
 
 # block_15_right = np.array([
@@ -117,10 +117,8 @@ block_15_right = np.array([
 # [b,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w,w]
 # ])
 
-
-# frame = cv2.imread("images/lanestill.png")
-# print(frame)
-
+leftblob = np.empty((20*15, 300))
+rightblob = np.empty((20*15, 300))
 while cap:
     ret, frame = cap.read()
     ysize = frame.shape[0]
@@ -131,14 +129,19 @@ while cap:
     start_time = time.time()
     scanwidth = 300
     scanheight = 15
+    scanwidthl = 300
+    scanwidthr = 300
 ####### main process loop
     for x in range(0,20):
-        leftscan = gray[leftindex[1]:leftindex[1]+scanheight , leftindex[0]:leftindex[0] + scanwidth]
-        rightscan = gray[rightindex[1]:rightindex[1]+scanheight , rightindex[0]:rightindex[0] + scanwidth]
+        leftscan = gray[leftindex[1]:leftindex[1]+scanheight , leftindex[0]:leftindex[0] + scanwidthl]
+        rightscan = gray[rightindex[1]:rightindex[1]+scanheight , rightindex[0]:rightindex[0] + scanwidthr]
         left = scipy.ndimage.convolve(leftscan, block_15_left)
         right = scipy.ndimage.convolve(rightscan, block_15_right)
-        next = 0
 
+        np.copyto(leftblob[x*15:x*15+15, 0:left.shape[1]], left)
+        np.copyto(rightblob[x*15:x*15+15, 0:right.shape[1]], right)
+        # print(left) 
+        # print(right)
         # print(left)
         # idx = np.argmax(left)
         # print("")
@@ -146,35 +149,51 @@ while cap:
         # print(left[idx%500, int(idx/500)])
         # find the maximum and draw a rectangle!
         # try:
+        # so idxL/R is the index of the max thing, or the best boundary location as an x offset from the scan box width
+        # idxLRf is the location of the box in the frame
+        # leftindex and rightindex are the top left point of the scan box.
         if True:
-            idxl = np.argmax(left)
-            idxr = np.argmax(right)
-            idxl = [idxl % scanwidth, 0]
-            idxr = [idxr % scanwidth, 0]
+            idxl = np.argmax(left[7])
+            idxr = np.argmax(right[7])
             # print("left max:"+str(idxl))
             # print("right max:"+str(idxr))
             green = (0,255,0)
             red = (0,0,255)
             blue = (255,0,0)
             rect = 15
-            cv2.rectangle(right, tuple(idxr), (idxr[0] + rect, idxr[1] + rect), 255)
-            cv2.rectangle(left, tuple(idxl), (idxl[0] + rect, idxl[1] + rect), 255)
-            idxlf = (idxl[0]+leftindex[0], idxl[1]+leftindex[1])
-            idxrf = (idxr[0]+rightindex[0] , idxr[1]+rightindex[1])
+            # cv2.rectangle(right, (idxr, 0), (idxr + rect, rect), 255)
+            # cv2.rectangle(left, (idxl, 0), (idxl + rect, rect), 255)
+            idxlf = (idxl + leftindex[0], leftindex[1])
+            idxrf = (idxr + rightindex[0] , rightindex[1])
             # print("left at frame loc:"+str(idxlf))
             # print("right at frame loc:"+str(idxrf))
-            cv2.rectangle(frame, tuple(leftindex), (leftindex[0] + scanwidth, leftindex[1] + scanheight), green, 2)
-            cv2.rectangle(frame, tuple(rightindex), (rightindex[0] + scanwidth, rightindex[1] + scanheight), green, 2)
+            cv2.rectangle(frame, tuple(leftindex), (leftindex[0] + scanwidthl, leftindex[1] + scanheight), green, 2)
+            cv2.rectangle(frame, tuple(rightindex), (rightindex[0] + scanwidthr, rightindex[1] + scanheight), green, 2)
             cv2.rectangle(frame, idxlf, (idxlf[0] + rect, idxlf[1] + rect), red, 3)
             cv2.rectangle(frame, idxrf, (idxrf[0] + rect, idxrf[1] + rect), blue, 3)
             # move the bounding box to next position
-            scanwidth = 100
-            leftindex = [idxlf[0] - int(scanwidth/2), idxlf[1]-15]
-            rightindex = [idxrf[0] - int(scanwidth/2), idxrf[1]-15]
+            if left[7][idxl] < 50:
+                scanwidthl = 300
+                # print("left poop")
+                leftindex = [leftindex[0], leftindex[1] - 15]
+            else:
+                scanwidthl = 100
+                leftindex = [idxlf[0] - int(scanwidthl/2), idxlf[1]-15]
+
+            if right[7][idxr] < 50:
+                scanwidthr = 300
+                # print("right poop")
+                rightindex = [rightindex[0], rightindex[1]-15]
+            else:        
+                scanwidthr = 100
+                rightindex = [idxrf[0] - int(scanwidthr/2), idxrf[1]-15]
+
+            # rightindex = [idxrf[0] - int(scanwidthr/2), idxrf[1]-15]
+            # leftindex = [idxlf[0] - int(scanwidthl/2), idxlf[1]-15]
             if leftindex[0] < 0:
                 leftindex[0] = 0
-            if rightindex[0] > xsize-scanwidth:
-                rightindex[0] = xsize-scanwidth
+            if rightindex[0] > xsize-scanwidthr:
+                rightindex[0] = xsize-scanwidthr
             # print("new left index:"+str(leftindex))
             # print("new right index:"+str(rightindex))
         # except: 
@@ -196,8 +215,8 @@ while cap:
     #time it from here
 
     cv2.imshow('frame', frame)
-    cv2.imshow('left', left)
-    cv2.imshow('right', right)
+    cv2.imshow('left', leftblob)
+    cv2.imshow('right', rightblob)
 
     # show the frame
     #cv2.imshow("Frame", frame)
