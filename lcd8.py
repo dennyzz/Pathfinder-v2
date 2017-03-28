8ccb9a6579a566ef05c46a69d842e4937e687f84
Denny <dennyz@umich.edu>
2017-03-28 17:43:13 -0400

    lcd update with correct pins

--- lcd8.py
+++ lcd8.py
@@ -4,10 +4,24 @@ from subprocess import *
 from time import sleep, strftime
 from datetime import datetime
 import Adafruit_CharLCD
+import Adafruit_GPIO as GPIO
 import time
 import string
 
-lcd = Adafruit_CharLCD(2,14,3,4,17,27,22,16,2)
+rs = 26
+rw = 19
+en = 13
+d4 = 12
+d5 = 16
+d6 = 20
+d7 = 21
+cols = 8
+lines = 2
+
+lcd = Adafruit_CharLCD(rs, en, d4, d5, d6, d7, cols, lines)
+gpio=GPIO.get_platform_gpio()
+gpio.setup(rw, GPIO.OUT)
+gpio.output(rw, False)
 lcd.clear()
 
 cmd = "ip addr show wlan0 | grep -m 1 inet | awk '{print $2}' | cut -d/ -f1"
