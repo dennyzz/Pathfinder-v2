c8075157444343827d68048bb6c7847f0419f85c
Pathfinder Project <Pathfinder@umich.edu>
2017-03-29 00:15:03 -0400

    fix some lcd8 things finally workng

--- lcd8.py
+++ lcd8.py
@@ -19,12 +19,12 @@ v0 = 6
 cols = 8
 lines = 2
 
-lcd = Adafruit_CharLCD.Adafruit_CharLCD(rs, en, d4, d5, d6, d7, cols, lines)
 gpio=GPIO.get_platform_gpio()
 gpio.setup(rw, GPIO.OUT)
-gpio.setup(v0, GPIO.OUT)
-gpio.output(v0, False)
 gpio.output(rw, False)
+gpio.setup(v0, GPIO.OUT)
+sleep(1)
+lcd = Adafruit_CharLCD.Adafruit_CharLCD(rs, en, d4, d5, d6, d7, cols, lines)
 lcd.clear()
 
 cmd = "ip addr show wlan0 | grep -m 1 inet | awk '{print $2}' | cut -d/ -f1"
@@ -35,10 +35,14 @@ def run_cmd(cmd):
         output = p.communicate()[0]
         return output
 
-while 1:
+while True:
         lcd.home()
         ipaddr = run_cmd(cmd)
         ips = string.split(ipaddr, '.')
-        lcd.message('%s.%s\n' % (ips[0], ips[1]) )
-        lcd.message('%s.%s' % (ips[2], ips[3]) )
+        if len(ips) == 4:
+                lcd.message('i%s.%s\n' % (ips[0], ips[1]) )
+                lcd.message('p%s.%s' % (ips[2], ips[3]) )
+        else:
+                lcd.message('   no   \n')
+                lcd.message('network!')
         sleep(1)
