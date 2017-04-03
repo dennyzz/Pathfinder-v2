d4af2bbee127b1fb02f782d1ce23c9f948af9bc8
Denny Zhang <dennyz@umich.edu>
2017-04-02 21:23:23 -0400

    update pathfinder shield

--- pathfindershield.py
+++ pathfindershield.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 
-#define PCA9685_MODE1 0x0
-#define PCA9685_PRESCALE 0xFE
-
 import smbus
 import sys
 
 
 # Usage: 
-
+# [0] status? blank
+# [1] MOTORA_CTRL
+# [2] MOTORA_SPEED
+# [3] MOTORB_CTRL
+# [4] MOTORB_SPEED
+# [5] Servo1
+# [6] Servo2
+# [7] LEDs
 
 def motorservocmd1(Aspeed, Adir, Abrake, Bspeed, Bdir, Bbrake, Servo1, Servo2):
 
     bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     DEVICE_ADDRESS = 0x5A      #7 bit address (will be left shifted to add the read write bit)
-    MOTOR_CMD_1 = 0x01
+    REG = 0x01
 
     CTRLA = 0
     CTRLB = 0
@@ -34,14 +38,14 @@ def motorservocmd1(Aspeed, Adir, Abrake, Bspeed, Bdir, Bbrake, Servo1, Servo2):
 
     cmd = {CTRLA, Aspeed, CTRLB, Bspeed, Servo1, Servo2}
 
-    bus.write_i2c_block_data(DEVICE_ADDRESS, MOTOR_CMD_1, cmd)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, REG, cmd)
 
 
 def motorservocmd2(Aspeed, Adir, Abrake, Bspeed, Bdir, Bbrake):
 
     bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     DEVICE_ADDRESS = 0x5A      #7 bit address (will be left shifted to add the read write bit)
-    MOTOR_CMD_2 = 0x02
+    REG = 0x01
 
     CTRLA = 0
     CTRLB = 0
@@ -59,15 +63,15 @@ def motorservocmd2(Aspeed, Adir, Abrake, Bspeed, Bdir, Bbrake):
 
     cmd = {CTRLA, Aspeed, CTRLB, Bspeed}
 
-    bus.write_i2c_block_data(DEVICE_ADDRESS, MOTOR_CMD_2, cmd)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, REG, cmd)
 
 
 def motorservocmd3(Aspeed, Adir, Abrake, Servo1):
 
     bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     DEVICE_ADDRESS = 0x5A      #7 bit address (will be left shifted to add the read write bit)
-    MOTOR_CMD_3 = 0x03
-
+    REG = 0x01
+    REG_SERVO1 = 0x05
     CTRLA = 0
     if Adir:
         CTRLA |= 1<<0
@@ -77,15 +81,17 @@ def motorservocmd3(Aspeed, Adir, Abrake, Servo1):
     assert Aspeed < 256 & Aspeed >=0
     assert Servo1 < 256 & Servo1 >=0
 
-    cmd = [CTRLA, Aspeed, Servo1]
+    cmd1 = [CTRLA, Aspeed]
+    cmd2 = Servo
 
-    bus.write_i2c_block_data(DEVICE_ADDRESS, MOTOR_CMD_3, cmd)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, REG, cmd1)
+    bus.write_byte_data(DEVICE_ADDRESS, REG_SERVO1, cmd2)
 
 def motorservocmd4(Bspeed, Bdir, Bbrake, Servo2):
 
     bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     DEVICE_ADDRESS = 0x5A      #7 bit address (will be left shifted to add the read write bit)
-    MOTOR_CMD_4 = 0x04
+    REG_MOTB = 0x01
 
     CTRLB = 0
     if Bdir:
@@ -96,14 +102,16 @@ def motorservocmd4(Bspeed, Bdir, Bbrake, Servo2):
     #assert (Bspeed < 256 & Bspeed >=0)
     #assert (Servo2 < 256 & Servo2 >=0)
 
-    cmd = [CTRLB, Bspeed, Servo2]
+    cmd1 = [CTRLB, Bspeed]
+    cmd2 = Servo2
 
-    bus.write_i2c_block_data(DEVICE_ADDRESS, MOTOR_CMD_4, cmd)
+    bus.write_i2c_block_data(DEVICE_ADDRESS, REG_MOTB, cmd1)
+    bus.write_byte_data(DEVICE_ADDRESS, REG_SERVO2, cmd2)
 
 
 def motorservoledcmd(leds):
     bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
     DEVICE_ADDRESS = 0x5A      #7 bit address (will be left shifted to add the read write bit)
-    LED_CMD = 0x10
+    REG_LED = 0x07
 
-    bus.write_byte_data(DEVICE_ADDRESS, LED_CMD, leds)
+    bus.write_byte_data(DEVICE_ADDRESS, REG_LED, leds)
